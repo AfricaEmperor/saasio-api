d2a54ae45609cdff409b7bb1fa3e3f63cd9b6413
AfricaEmperor <bigconsulting@gmail.com>
2025-06-11 03:27:36 +0100

    Fix 404 error

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {"message": "SaaSIO MVP API Online"}
+
+# Add this block
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
